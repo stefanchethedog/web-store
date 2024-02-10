073e46db549ff1a329d0ef7c837b8f86cc69304f
Uros Lazic <uros.lazic@protonmail.com>
2024-02-10 18:26:19 +0100

    code cleanup

--- frontend/src/pages/Schema/Update/UpdateSchema.tsx
+++ frontend/src/pages/Schema/Update/UpdateSchema.tsx
@@ -1,6 +1,5 @@
 import { Error } from '@mui/icons-material';
-import { Checkbox, TextField, Select, Button, MenuItem } from '@mui/material';
-import { Stack } from '@mui/system';
+import { Checkbox, TextField, Select, Button, MenuItem, Stack } from '@mui/material';
 import axios from 'axios';
 import { FC, useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
@@ -32,97 +31,87 @@ const UpdateSchema: FC<UpdateSchemaProps> = ({ }) => {
   const schemaName = parts[parts.length - 1];
 
   useEffect(() => {
-    axios.get<ISchemaResponse>(GET_SCHEMA_BY_NAME(schemaName)).then((response) => {
-      setSchema(response.data);
-    }).catch((reason) => {
-      setError(reason);
-    })
+    axios
+      .get<ISchemaResponse>(GET_SCHEMA_BY_NAME(schemaName))
+      .then((response) => {
+        setSchema(response.data);
+      })
+      .catch((reason) => {
+        setError(reason);
+      })
   }, [])
+
+  const updateKeys = <T,>(keyName: string, propName: string, value: T) => {
+    return schema!.keys.map((k) => {
+      if (k.name === keyName) {
+        return { ...k, [propName]: value };
+      }
+      else return k;
+    });
+  }
+
+  const handleKeyChange = <T,>(keyName: string, propName: string, value: T) => {
+    setSchema({
+      ...schema!,
+      keys: updateKeys(keyName, propName, value)
+    });
+  }
 
   return (
     <div className="update-schema__container">
       {error && <Error>{error}</Error>}
-      Update {schemaName}
-      <Stack p="20px" gap="10px">
-        <TextField
-          label="Name"
-          value={schema?.name ?? ""}
-          onChange={(e) => {
-            setSchema({ ...schema!, name: e.target.value })
-          }}
-        >
-        </TextField>
-        {schema?.keys.map((key) => {
-          return (
-            <Stack direction="row">
-              <TextField
-                value={key.name}
-                onChange={(e) => {
-                  const updatedKeys = schema.keys.map((k) => {
-                    if (k.name === key.name) {
-                      return { ...k, name: e.target.value };
-                    }
-                    else return k;
-                  });
-                  setSchema({
-                    ...schema,
-                    keys: updatedKeys
-                  })
-                }}
-              >
-              </TextField>
-              <Select
-                label={key.name}
-                value={key.type.toLowerCase() ?? ""}
-                onChange={(e) => {
-                  const updatedKeys = schema.keys.map((k) => {
-                    if (k.name === key.name) {
-                      return { ...k, type: e.target.value };
-                    }
-                    else return k;
-                  });
-                  setSchema({
-                    ...schema,
-                    keys: updatedKeys
-                  })
-                }}
-              >
-                {["string", "number"].map((type) => {
-                  return (
-                    <MenuItem value={type}>{type}</MenuItem>
-                  );
-                })}
-              </Select>
-              <Checkbox
-                checked={key.required}
-                onChange={(e) => {
-                  const updatedKeys = schema.keys.map((k) => {
-                    if (k.name === key.name) {
-                      return { ...k, required: e.target.checked };
-                    }
-                    else return k;
-                  });
-                  setSchema({
-                    ...schema,
-                    keys: updatedKeys
-                  })
-                }}
-              />
-            </Stack>
-          );
-        })}
-      </Stack>
-      <Button
-        variant="contained"
-        onClick={() => {
-          axios
-            .put(UPDATE_SCHEMA_BY_NAME(schemaName), { ...schema })
-            .then((res) => console.log(res))
-            .catch((err) => console.error(err));
-        }}
-      >
-        Update
-      </Button>
+      {schema &&
+        <>
+          Update {schemaName}
+          <Stack p="20px" gap="10px">
+            <TextField
+              label="Name"
+              value={schema.name ?? ""}
+              onChange={(e) => {
+                setSchema({ ...schema, name: e.target.value })
+              }}
+            >
+            </TextField>
+            {schema.keys.map((key) => {
+              return (
+                <Stack direction="row">
+                  <TextField
+                    value={key.name}
+                    onChange={(e) => handleKeyChange(key.name, "name", e.target.value)}
+                  >
+                  </TextField>
+                  <Select
+                    label={key.name}
+                    value={key.type.toLowerCase() ?? ""}
+                    onChange={(e) => handleKeyChange(key.name, "type", e.target.value)}
+                  >
+                    {["string", "number"].map((type) => {
+                      return (
+                        <MenuItem value={type}>{type}</MenuItem>
+                      );
+                    })}
+                  </Select>
+                  <Checkbox
+                    checked={key.required}
+                    onChange={(e) => handleKeyChange(key.name, "required", e.target.value)}
+                  />
+                </Stack>
+              );
+            })}
+          </Stack>
+          <Button
+            variant="contained"
+            onClick={() => {
+              axios
+                .put(UPDATE_SCHEMA_BY_NAME(schemaName), { ...schema })
+                .then((res) => console.log(res))
+                .catch((err) => console.error(err));
+            }}
+          >
+            Update
+          </Button>
+        </>
+      }
     </div>
   );
 };
